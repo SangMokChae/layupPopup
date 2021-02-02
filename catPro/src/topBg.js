21c8d48af5cee5109b657c0d1738d71cdd80afb9
sangmokchae <72491390+SangMokChae@users.noreply.github.com>
2021-02-02 21:54:31 +0900

    project-file
    시험용

---
+++ catPro/src/topBg.js
@@ -0,0 +1,16 @@
+let bgArray = new Array();
+bgArray[0] = "4.jpg";
+bgArray[1] = "5.jpg";
+bgArray[2] = "6.jpg";
+bgArray[3] = "7.jpg";
+
+function showImg() {
+  let imgNum = Math.round(Math.random()*3);
+  let objImg = document.querySelector("#bgPhoto");
+  objImg.src=`/img/${bgArray[imgNum]}`;
+}
+
+function init() {
+  setInterval(showImg, 6000);
+}
+init();
